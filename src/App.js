e352791d4eb129bb82b3dd47ab6372aa4e9cfe7c
Loang_khan <solangisohailkhan903@gmail.com>
2023-09-24 20:54:51 +0500

    APIs fetched using axios

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import './App.css';
 import {useState, useEffect} from 'react'
 import Post from './components/Post';
 import AddPost from './components/AddPost';
+import axios from 'axios'
 
 function App() {
 
 const [posts, setPosts] = useState([]);
 
-const fetchPosts = () => {
-  fetch('https://jsonplaceholder.typicode.com/posts?_limit=4')
-  .then ((response) => response.json())
-  .then((data) => setPosts(data))
+const client = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com/posts'
+});
+
+const fetchPosts = async() => {
+ try{
+  const response = await client.get('?_limit=4')
+  setPosts(response.data);
+ } catch (error) {
+  console.log(error);
+ }
+  
 }
 
 useEffect(() => {
@@ -18,38 +27,53 @@ useEffect(() => {
 }, []);
 
 
-const addPost = (title, body) => {
-  fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      body: JSON.stringify({
-          title: title,
-          body: body,
-          userId: Math.random().toString(36).slice(2),
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      setPosts((prevPosts) => [data, ...prevPosts])
-    
-    })
+const addPost = async(title, body) => {
+
+    // this is code using fetch method to consume APIs
+
+  // const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //         title: title,
+  //         body: body,
+  //         userId: Math.random().toString(36).slice(2),
+  //     }),
+  //     headers: {
+  //       'Content-type': 'application/json; charset=UTF-8',
+  //   },
+  // })
+  //  const data = response.json();
+  //     setPosts((prevPosts) => [data, ...prevPosts])
+
+//------------ fetching data using axios for APIs
+
+  const response = await client.post('', {
+    title,
+    body
+  });
+  setPosts((prevPosts) => [response.data, ...prevPosts]);
 };
 
-const deletePost = (id) => {
-  fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-      method: 'DELETE'
-    })
-    .then((response) => {
-      if(response.status === 200) {
-        setPosts(
-          posts.filter((post) => {
-            return post.id !== id;
-          })
-        )
-      }
-    })
+const deletePost = async(id) => {
+  // code using fetch with APIs to delete data
+  
+  // const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+  //     method: 'DELETE'
+  //   })
+  //     if(response.status === 200) {
+  //       setPosts(
+  //         posts.filter((post) => {
+  //           return post.id !== id;
+  //         })
+  //       )
+  //     }
+
+  //-------------- here is code to use Axios with APIs
+
+  const response = await client.delete(`${id}`)
+  setPosts(posts.filter((post) => {
+    return post.id !== id;
+  }))
 };
 
   return (
